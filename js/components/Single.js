c3d9ec71573a41278167133ae913a874ae38a232
Vitor Fratti <vifratti@gmail.com>
2023-08-02 23:48:03 -0300

    [ ROUTER/SINGLE ] Single dos projetos funcionando com Vue Router

---
+++ js/components/Single.js
@@ -0,0 +1,62 @@
+const Single = {
+    template: `
+    <section class="single-projeto" id="single-projeto">
+        <div class="container" v-for="(single, index) in single" :key="index">
+            <div class="top">
+                <router-link to="/"><h4><span><- </span>Projetos</h4></router-link>
+                <h2>{{ single.nome }}</h2>
+                <p>{{ single.descricao }}</p>
+            </div>
+            <div class="mid">
+                <div class="left">
+                    <div class="tools" v-if="single.status == false">
+                        <img :src="projeto.src" alt="example">
+                    </div>
+                    <div v-else class="single-projeto_box">
+                        <img :src="single.src" alt="example">
+                    </div>
+                    <div class="action-buttons">
+                        <a :href="single.site" target="_blank" class="site">Ir para o site</a>
+                        <a v-if="single.categoria == 'Corporativo'" :href="single.repositorio" target="_blank" title="Projeto Corporativo" class="repositorio disabled" disabled>
+                            Ver repositório
+                            <img src="image/svg/blocked.svg" alt="blocked">
+                        </a>
+                        <a v-else class="repositorio" :href="single.repositorio" target="_blank">Ver repositório</a>
+                    </div>
+                </div>
+                <div class="right">
+                    <div class="title">
+                        <h2>Conhecimentos utilizados:</h2>
+                    </div>
+                    <div class="conhecimentos">
+                        <span v-for="(conhecimento, index) in single.conhecimentos" :key="index" @mouseover="single.conhecimentoActive = []; single.conhecimentoActive.push(conhecimento)" @click="single.conhecimentoActive = []; single.conhecimentoActive.push(conhecimento)">
+                            <img :src="conhecimento.src" alt="">
+                        </span>
+                    </div>
+                    <div class="conhecimentos_text">
+                        <span v-for="(conhecimento, index) in single.conhecimentoActive" :key="index">
+                            <h3>{{ conhecimento.name }}</h3>
+                            <h4 v-if="conhecimento.level === 'Avançado'" class="avancado">{{ conhecimento.level }}</h4>
+                            <h4 v-if="conhecimento.level === 'Intermediário'" class="intermediario">{{ conhecimento.level }}</h4>
+                            <h4 v-if="conhecimento.level === 'Básico'" class="basico">{{ conhecimento.level }}</h4>
+                            <p>{{ conhecimento.descricao }}</p>
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div class="bottom">
+                <router-link to="/"><h4><span><- </span>Voltar para Projetos</h4></router-link>
+            </div>
+        </div>
+    </section>
+    `,
+    data() {
+        return {}
+    },
+    mounted() {
+        this.single[0].conhecimentoActive.push(this.single[0].conhecimentos[0])
+    },
+    props: ['single']
+};
+
+export default Single;
--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+import Single from './components/Single.js';
+
+const routes = [
+    { path: '/single', component: Single },
+]
+
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHashHistory(),
+    routes
+})
+
 const app = Vue.createApp({
     data() {
         return {
@@ -8,25 +19,77 @@ const app = Vue.createApp({
             projetos: [
                 {
                     nome: 'Ecoa Energias Renováveis',
-                    descricao: 'Site institucional, feito em: jQuery, Sass, PHP, WordPress',
+                    descricao: 'Site institucional, feito em: jQuery, Sass, PHP, WordPress.',
                     src: 'image/Ecoa.png',
                     buttons: false,
                     status: true,
                     categoria: 'Corporativo',
                     site: null,
                     repositorio: null,
-                    conhecimentos: ['jQuery', 'Sass', 'PHP', 'WordPress']
+                    conhecimentos: [
+                        {
+                            name: 'jQuery',
+                            level: 'Intermediário',
+                            descricao: 'jQuery é uma biblioteca JavaScript de alto desempenho que simplifica a manipulação e interação com elementos HTML, tratamento de eventos, animações e requisições assíncronas. Com uma sintaxe concisa e poderosa, o jQuery agiliza o desenvolvimento web e garante compatibilidade com diferentes navegadores.',
+                            src: 'image/svg/jquery.svg'
+                        },
+                        {
+                            name: 'Sass',
+                            level: 'Avançado',
+                            descricao: 'Sass é uma linguagem de folhas de estilo que estende o CSS. Ele oferece recursos adicionais, como variáveis, mixins e aninhamento de seletores, que tornam a escrita de estilos mais eficiente e modular. O Sass é compilado para CSS, permitindo que os estilos sejam utilizados pelos navegadores.',
+                            src: 'image/svg/sass.svg'
+                        },
+                        {
+                            name: 'PHP',
+                            level: 'Intermediário',
+                            descricao: 'PHP é uma linguagem de programação do lado do servidor, usada principalmente para criar páginas da web dinâmicas. Com o PHP, é possível interagir com bancos de dados, processar formulários, gerar conteúdo personalizado e muito mais. Sua sintaxe é semelhante ao HTML, permitindo a incorporação fácil de código PHP em arquivos HTML.',
+                            src: 'image/svg/php.svg'
+                        },
+                        {
+                            name: 'WordPress',
+                            level: 'Intermediário',
+                            descricao: 'WordPress é uma plataforma de gerenciamento de conteúdo (CMS) popular e de código aberto, usada para criar e gerenciar sites e blogs. Com recursos flexíveis e uma grande variedade de plugins e temas, o WordPress permite criar sites personalizados de forma acessível e amigável.',
+                            src: 'image/svg/wordpress.svg'
+                        }
+                    ],
+                    conhecimentoActive: []
                 },
                 {
                     nome: 'VillaReal Marina',
-                    descricao: 'Site institucional, feito em: jQuery, Sass, PHP, WordPress',
+                    descricao: 'Site institucional, feito em: jQuery, Sass, PHP, WordPress.',
                     src: 'image/Home - VillaReal Marina.png',
                     buttons: false,
                     status: true,
                     categoria: 'Corporativo',
                     site: 'https://marinavillareal.com.br/',
                     repositorio: null,
-                    conhecimentos: ['jQuery', 'Sass', 'PHP', 'WordPress']
+                    conhecimentos: [
+                        {
+                            name: 'jQuery',
+                            level: 'Intermediário',
+                            descricao: 'jQuery é uma biblioteca JavaScript de alto desempenho que simplifica a manipulação e interação com elementos HTML, tratamento de eventos, animações e requisições assíncronas. Com uma sintaxe concisa e poderosa, o jQuery agiliza o desenvolvimento web e garante compatibilidade com diferentes navegadores.',
+                            src: 'image/svg/jquery.svg'
+                        },
+                        {
+                            name: 'Sass',
+                            level: 'Avançado',
+                            descricao: 'Sass é uma linguagem de folhas de estilo que estende o CSS. Ele oferece recursos adicionais, como variáveis, mixins e aninhamento de seletores, que tornam a escrita de estilos mais eficiente e modular. O Sass é compilado para CSS, permitindo que os estilos sejam utilizados pelos navegadores.',
+                            src: 'image/svg/sass.svg'
+                        },
+                        {
+                            name: 'PHP',
+                            level: 'Intermediário',
+                            descricao: 'PHP é uma linguagem de programação do lado do servidor, usada principalmente para criar páginas da web dinâmicas. Com o PHP, é possível interagir com bancos de dados, processar formulários, gerar conteúdo personalizado e muito mais. Sua sintaxe é semelhante ao HTML, permitindo a incorporação fácil de código PHP em arquivos HTML.',
+                            src: 'image/svg/php.svg'
+                        },
+                        {
+                            name: 'WordPress',
+                            level: 'Intermediário',
+                            descricao: 'WordPress é uma plataforma de gerenciamento de conteúdo (CMS) popular e de código aberto, usada para criar e gerenciar sites e blogs. Com recursos flexíveis e uma grande variedade de plugins e temas, o WordPress permite criar sites personalizados de forma acessível e amigável.',
+                            src: 'image/svg/wordpress.svg'
+                        }
+                    ],
+                    conhecimentoActive: []
                 },
                 {
                     nome: 'Em desenvolvimento',
@@ -51,7 +114,45 @@ const app = Vue.createApp({
                     conhecimentos: []
                 }
             ],
-            projetoSingle: [],
+            single: [
+                {
+                    nome: 'Ecoa Energias Renováveis',
+                    descricao: 'Site institucional, feito em: jQuery, Sass, PHP, WordPress.',
+                    src: 'image/Ecoa.png',
+                    buttons: false,
+                    status: true,
+                    categoria: 'Corporativo',
+                    site: null,
+                    repositorio: null,
+                    conhecimentos: [
+                        {
+                            name: 'jQuery',
+                            level: 'Intermediário',
+                            descricao: 'jQuery é uma biblioteca JavaScript de alto desempenho que simplifica a manipulação e interação com elementos HTML, tratamento de eventos, animações e requisições assíncronas. Com uma sintaxe concisa e poderosa, o jQuery agiliza o desenvolvimento web e garante compatibilidade com diferentes navegadores.',
+                            src: 'image/svg/jquery.svg'
+                        },
+                        {
+                            name: 'Sass',
+                            level: 'Avançado',
+                            descricao: 'Sass é uma linguagem de folhas de estilo que estende o CSS. Ele oferece recursos adicionais, como variáveis, mixins e aninhamento de seletores, que tornam a escrita de estilos mais eficiente e modular. O Sass é compilado para CSS, permitindo que os estilos sejam utilizados pelos navegadores.',
+                            src: 'image/svg/sass.svg'
+                        },
+                        {
+                            name: 'PHP',
+                            level: 'Intermediário',
+                            descricao: 'PHP é uma linguagem de programação do lado do servidor, usada principalmente para criar páginas da web dinâmicas. Com o PHP, é possível interagir com bancos de dados, processar formulários, gerar conteúdo personalizado e muito mais. Sua sintaxe é semelhante ao HTML, permitindo a incorporação fácil de código PHP em arquivos HTML.',
+                            src: 'image/svg/php.svg'
+                        },
+                        {
+                            name: 'WordPress',
+                            level: 'Intermediário',
+                            descricao: 'WordPress é uma plataforma de gerenciamento de conteúdo (CMS) popular e de código aberto, usada para criar e gerenciar sites e blogs. Com recursos flexíveis e uma grande variedade de plugins e temas, o WordPress permite criar sites personalizados de forma acessível e amigável.',
+                            src: 'image/svg/wordpress.svg'
+                        }
+                    ],
+                    conhecimentoActive: []
+                },
+            ],
             conhecimentos: [
                 {
                     name: 'HTML',
@@ -136,6 +237,9 @@ const app = Vue.createApp({
             ]
         }
     },
+    components: {
+        Single
+    },
     methods: {
         changeSection() {
             const element = document.getElementById(this.section);
@@ -149,9 +253,8 @@ const app = Vue.createApp({
             document.querySelector('.menu-lateral').classList.toggle('closed')
         },
         showProject(index) {
-            this.projetoSingle = []
-            this.projetoSingle.push(this.projetos[index])
-            window.location.href = "single-projeto.html";
+            this.single = []
+            this.single.push(this.projetos[index])
         },
         animateJumpLeft() {
             document.querySelector('.slick-prev').classList.add('animate__tada')
@@ -227,6 +330,8 @@ const app = Vue.createApp({
         });
     }
 })
+
+app.use(router)
 
 app.mount('#app')
 
